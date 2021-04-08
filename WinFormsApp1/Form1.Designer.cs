14fcdc9cc079cbe15e7df54c07984a4540a389ef
name <hg@joo>
2021-04-07 19:46:45 -0700

    initial commit

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Background_imager_form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.excerpt_params = new System.Windows.Forms.GroupBox();
+            this.keywords_textbox = new System.Windows.Forms.TextBox();
+            this.attribute_textbox = new System.Windows.Forms.TextBox();
+            this.excerpt_textbox = new System.Windows.Forms.TextBox();
+            this.get_images_button = new System.Windows.Forms.Button();
+            this.download_button = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList3 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList4 = new System.Windows.Forms.ImageList(this.components);
+            this.image_picture_box = new System.Windows.Forms.PictureBox();
+            this.first_button = new System.Windows.Forms.Button();
+            this.next_button = new System.Windows.Forms.Button();
+            this.last_button = new System.Windows.Forms.Button();
+            this.back_button = new System.Windows.Forms.Button();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.user_docs = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.excerpt_params.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.image_picture_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // excerpt_params
+            // 
+            this.excerpt_params.Controls.Add(this.keywords_textbox);
+            this.excerpt_params.Controls.Add(this.attribute_textbox);
+            this.excerpt_params.Controls.Add(this.excerpt_textbox);
+            this.excerpt_params.Location = new System.Drawing.Point(44, 37);
+            this.excerpt_params.Name = "excerpt_params";
+            this.excerpt_params.Size = new System.Drawing.Size(416, 311);
+            this.excerpt_params.TabIndex = 0;
+            this.excerpt_params.TabStop = false;
+            this.excerpt_params.Text = "Quotes Parameters";
+            // 
+            // keywords_textbox
+            // 
+            this.keywords_textbox.Location = new System.Drawing.Point(219, 269);
+            this.keywords_textbox.Name = "keywords_textbox";
+            this.keywords_textbox.PlaceholderText = "Indicate keyword here";
+            this.keywords_textbox.Size = new System.Drawing.Size(191, 23);
+            this.keywords_textbox.TabIndex = 2;
+            // 
+            // attribute_textbox
+            // 
+            this.attribute_textbox.Location = new System.Drawing.Point(7, 269);
+            this.attribute_textbox.Name = "attribute_textbox";
+            this.attribute_textbox.PlaceholderText = "Indicate your attribution here";
+            this.attribute_textbox.Size = new System.Drawing.Size(197, 23);
+            this.attribute_textbox.TabIndex = 1;
+            // 
+            // excerpt_textbox
+            // 
+            this.excerpt_textbox.Location = new System.Drawing.Point(7, 22);
+            this.excerpt_textbox.Multiline = true;
+            this.excerpt_textbox.Name = "excerpt_textbox";
+            this.excerpt_textbox.PlaceholderText = "Write/Paste your quote here";
+            this.excerpt_textbox.Size = new System.Drawing.Size(403, 241);
+            this.excerpt_textbox.TabIndex = 0;
+            this.excerpt_textbox.TextChanged += new System.EventHandler(this.excerpt_textbox_TextChanged);
+            // 
+            // get_images_button
+            // 
+            this.get_images_button.Location = new System.Drawing.Point(208, 354);
+            this.get_images_button.Name = "get_images_button";
+            this.get_images_button.Size = new System.Drawing.Size(246, 38);
+            this.get_images_button.TabIndex = 1;
+            this.get_images_button.Text = "Get Images";
+            this.get_images_button.UseVisualStyleBackColor = true;
+            this.get_images_button.Click += new System.EventHandler(this.get_images_button_click);
+            // 
+            // download_button
+            // 
+            this.download_button.Location = new System.Drawing.Point(563, 355);
+            this.download_button.Name = "download_button";
+            this.download_button.Size = new System.Drawing.Size(209, 38);
+            this.download_button.TabIndex = 3;
+            this.download_button.Text = "Download";
+            this.download_button.UseVisualStyleBackColor = true;
+            this.download_button.Click += new System.EventHandler(this.download_button_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList3
+            // 
+            this.imageList3.ColorDepth = System.Windows.Forms.ColorDepth.Depth4Bit;
+            this.imageList3.ImageSize = new System.Drawing.Size(4, 4);
+            this.imageList3.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList4
+            // 
+            this.imageList4.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList4.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList4.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // image_picture_box
+            // 
+            this.image_picture_box.Location = new System.Drawing.Point(563, 37);
+            this.image_picture_box.Name = "image_picture_box";
+            this.image_picture_box.Size = new System.Drawing.Size(209, 292);
+            this.image_picture_box.TabIndex = 4;
+            this.image_picture_box.TabStop = false;
+            // 
+            // first_button
+            // 
+            this.first_button.Location = new System.Drawing.Point(513, 72);
+            this.first_button.Name = "first_button";
+            this.first_button.Size = new System.Drawing.Size(44, 39);
+            this.first_button.TabIndex = 5;
+            this.first_button.Text = "First";
+            this.first_button.UseVisualStyleBackColor = true;
+            this.first_button.Click += new System.EventHandler(this.first_button_Click);
+            // 
+            // next_button
+            // 
+            this.next_button.Location = new System.Drawing.Point(513, 128);
+            this.next_button.Name = "next_button";
+            this.next_button.Size = new System.Drawing.Size(44, 39);
+            this.next_button.TabIndex = 6;
+            this.next_button.Text = "Next";
+            this.next_button.UseVisualStyleBackColor = true;
+            this.next_button.Click += new System.EventHandler(this.next_button_Click);
+            // 
+            // last_button
+            // 
+            this.last_button.Location = new System.Drawing.Point(513, 240);
+            this.last_button.Name = "last_button";
+            this.last_button.Size = new System.Drawing.Size(44, 39);
+            this.last_button.TabIndex = 7;
+            this.last_button.Text = "Last";
+            this.last_button.UseVisualStyleBackColor = true;
+            this.last_button.Click += new System.EventHandler(this.last_button_Click);
+            // 
+            // back_button
+            // 
+            this.back_button.Location = new System.Drawing.Point(513, 185);
+            this.back_button.Name = "back_button";
+            this.back_button.Size = new System.Drawing.Size(44, 39);
+            this.back_button.TabIndex = 8;
+            this.back_button.Text = "Back";
+            this.back_button.UseVisualStyleBackColor = true;
+            this.back_button.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // clear_button
+            // 
+            this.clear_button.Location = new System.Drawing.Point(51, 354);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(143, 37);
+            this.clear_button.TabIndex = 9;
+            this.clear_button.Text = "Clear";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // user_docs
+            // 
+            this.user_docs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.user_docs.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.user_docs.Location = new System.Drawing.Point(479, 37);
+            this.user_docs.Multiline = true;
+            this.user_docs.Name = "user_docs";
+            this.user_docs.ReadOnly = true;
+            this.user_docs.Size = new System.Drawing.Size(293, 311);
+            this.user_docs.TabIndex = 3;
+            this.user_docs.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // backgroundWorker1
+            // 
+
+            // 
+            // Background_imager_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.user_docs);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.back_button);
+            this.Controls.Add(this.last_button);
+            this.Controls.Add(this.next_button);
+            this.Controls.Add(this.first_button);
+            this.Controls.Add(this.image_picture_box);
+            this.Controls.Add(this.download_button);
+            this.Controls.Add(this.get_images_button);
+            this.Controls.Add(this.excerpt_params);
+            this.Name = "Background_imager_form";
+            this.Text = "Background Imager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.excerpt_params.ResumeLayout(false);
+            this.excerpt_params.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.image_picture_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox excerpt_params;
+        private System.Windows.Forms.TextBox keywords_textbox;
+        private System.Windows.Forms.TextBox attribute_textbox;
+        private System.Windows.Forms.TextBox excerpt_textbox;
+        private System.Windows.Forms.Button get_images_button;
+        private System.Windows.Forms.Button download_button;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList3;
+        private System.Windows.Forms.ImageList imageList4;
+        private System.Windows.Forms.PictureBox image_picture_box;
+        private System.Windows.Forms.Button first_button;
+        private System.Windows.Forms.Button next_button;
+        private System.Windows.Forms.Button last_button;
+        private System.Windows.Forms.Button back_button;
+        private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.TextBox user_docs;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Background_imager_form : Form
+    {
+        private string[] image_array;
+        int current_image_index = 0;
+        public Background_imager_form()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("sldkjsd");
+        }
+
+        private void get_images_button_click(object sender, EventArgs e)
+        {
+            String excerpt_string = get_excerpt();
+            String attributes_text = get_attribution_text();
+            String keywords_text = get_keywords();
+            System.Net.HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://flask-api-imager.herokuapp.com/get_background");
+
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string postData = "excerpt="+excerpt_string;
+            postData += "&attribution="+attributes_text;
+            postData += "&keywords=" + keywords_text;
+            byte[] data = encoding.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            try
+            {
+                // show progress bar
+                user_docs.Text = user_docs.Text + " Please wait while we process your request";
+                backgroundWorker1.WorkerReportsProgress = true;
+                backgroundWorker1.RunWorkerAsync();
+                fetch_images(request);
+                /**/
+
+            }
+            catch (Exception err)
+            {
+                get_images_button.BackColor = System.Drawing.Color.Red;
+                excerpt_textbox.Text = err.Message + "\n";
+            }
+
+        }
+
+        public void fetch_images(HttpWebRequest request)
+        {
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            String responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            this.image_array = responseString.Split('#');
+            load_image();
+            download_button.Visible = true;
+            next_button.Visible = true;
+            back_button.Visible = true;
+            first_button.Visible = true;
+            last_button.Visible = true;
+            image_picture_box.Visible = true;
+            user_docs.Visible = false;
+        }
+               
+
+
+        public string FixBase64ForImage(string Image)
+        {
+            System.Text.StringBuilder sbText = new System.Text.StringBuilder(Image, Image.Length);
+            sbText.Replace("\r\n", String.Empty); sbText.Replace(" ", String.Empty);
+            return sbText.ToString();
+        }
+
+        private void load_image(int index=0)
+        {
+            Byte[] bitmapData = Convert.FromBase64String(FixBase64ForImage(this.image_array[index]));
+            System.IO.MemoryStream streamBitmap = new System.IO.MemoryStream(bitmapData);
+            Bitmap bitImage = new Bitmap((Bitmap)Image.FromStream(streamBitmap));
+            image_picture_box.SizeMode = PictureBoxSizeMode.StretchImage;
+            image_picture_box.Image = bitImage;
+        }
+      
+        private String get_excerpt()
+        {
+            String excerpt = excerpt_textbox.Text.Trim();
+            if (excerpt.Length == 0)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+                MessageBox.Show("Quote cannot be empty. Using 'Hello World' instead", "Warning", buttons);
+                excerpt = "Hello World";                
+            }
+            return excerpt;
+        }
+
+        private String get_attribution_text()
+        {
+            return attribute_textbox.Text.Trim();
+        }
+
+        private String get_keywords()
+        {
+            return keywords_textbox.Text.Trim();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            download_button.Visible = false;
+            next_button.Visible = false;
+            back_button.Visible = false;
+            first_button.Visible = false;
+            last_button.Visible = false;
+            image_picture_box.Visible = false;
+            user_docs.Text = "With the Background Imager, You can add beautiful backgrounds to text. \n" +
+                "Just paste / write your text, and fill in the attributions(who said the words). \n " +
+                "You may also add spedcific keyword to use to perform your search. ";
+        }
+
+        private void images_list_view_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void results_box_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void excerpt_textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!is_index_ideal(false))
+            {
+                is_index_ideal(true);
+                load_image(this.current_image_index);
+            }
+            else
+            {
+                is_index_ideal(true);
+                this.current_image_index = this.current_image_index - 1;
+                load_image(this.current_image_index);
+            }
+        }
+
+        private void first_button_Click(object sender, EventArgs e)
+        {
+            this.current_image_index = 0;
+            is_index_ideal(false);
+            is_index_ideal(true);
+            this.load_image();
+        }
+
+        private void next_button_Click(object sender, EventArgs e)
+        {
+         
+            if(!is_index_ideal(true))
+            {
+                is_index_ideal(false);
+                load_image(this.current_image_index);
+            }
+            else
+            {
+                is_index_ideal(false);
+                this.current_image_index = this.current_image_index + 1;
+                load_image(this.current_image_index);
+            }
+        }
+
+        private void last_button_Click(object sender, EventArgs e)
+        {
+            this.current_image_index = this.image_array.Length - 1;
+            is_index_ideal(false);
+            is_index_ideal(true);
+            load_image(this.current_image_index);
+        }
+
+        private Boolean is_index_ideal(bool is_next_button)
+        {
+            int current_index = this.current_image_index;
+            if (is_next_button)
+            {
+                if (current_index + 1 >= this.image_array.Length)
+                {
+                    next_button.Enabled = false;
+                    return false;
+                }
+                else
+                {
+                    next_button.Enabled = true;
+                    return true;
+                }
+            }
+            else
+            {
+                if (current_index - 1 < 0)
+                {
+                    back_button.Enabled = false;
+                    return false;
+                }
+                else
+                {
+                    back_button.Enabled = true;
+                    return true;
+                }
+            }
+        }
+
+        private void download_button_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Images|*.png;*.bmp;*.jpg";
+            ImageFormat format = ImageFormat.Png;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(sfd.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                image_picture_box.Image.Save(sfd.FileName, format);
+            }
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            excerpt_textbox.Clear();
+            keywords_textbox.Clear();
+            attribute_textbox.Clear();
+        }
+
+    }
+}
